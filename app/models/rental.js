bc754a454175a65b4d7ec01c4bd9d3e436173506
vpetrenko <vpetrenko@mindk.com>
2016-04-23 19:29:31 +0300

    isntall mirage

---
+++ app/models/rental.js
@@ -0,0 +1,10 @@
+import Model from 'ember-data/model';
+
+export default Model.extend({
+	title: DS.attr(),
+	owner: DS.attr(),
+	city: DS.attr(),
+	type: DS.attr(),
+	image: DS.attr(),
+	bedrooms: DS.attr()
+});
